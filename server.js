dae2081567fa918f9498e8b06169b6c940e2e89a
Heathlington <126424577+Heathlington@users.noreply.github.com>
2023-05-18 13:48:44 -0500

    added files and coding

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const {readFile, writeFile} = require('fs/promises');
+const PORT = process.env.port || 3001;
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for feedback page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) =>{
+    readFile('db/db.json')  
+    .then(data => {
+        res.send(data)
+    })
+})
+
+app.post('/api/notes', (req, res) =>{
+    readFile('db/db.json')  
+    .then(data => {
+        const newNote = req.body 
+        newNote.id = uuidv4()
+        const parsedData = JSON.parse(data)
+        parsedData.push(newNote)
+        return writeFile('db/db.json', JSON.stringify(parsedData))
+    }).then(data => {
+        res.json(data)
+    })
+})
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
